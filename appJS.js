d6e90129006176ffc6d5ab45fbd4ec68f48ec90a
Bily Cuscul <billycuscul@gmail.com>
2018-11-08 21:01:59 -0600

    Clase2
    Clase Node JS practica de modulos

---
+++ appJS.js
@@ -0,0 +1,8 @@
+
+const miModulo = require('./Geometria/moduloCirculo');
+
+
+console.log(`Perimetro de un circulo cuando el radio es 10: ${miModulo.perimetro(10)}`);
+
+
+console.log(`Radio de un circulo cuando el perimetro es 5: ${miModulo.radio(5)}`);
