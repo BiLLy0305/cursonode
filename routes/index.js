c3a9d18e669e023a37401be59dc3ce4b94d58261
Bily Cuscul <billycuscul@gmail.com>
2018-11-13 20:58:59 -0600

    clase 3
    verbos http get, post, put, deltete

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+
+
+
+const lista = [1,2,3];
+
+module.exports = [
+    {
+        method: 'GET',
+        path :'/lista/{id}',
+        handler:function(request, h){
+            return {data: lista[request.params.id]};
+                 }
+    },
+    {
+    method: 'GET',
+    path :'/lista',
+    handler:function(request, h){
+        return {data: lista};
+             }
+    },
+    {
+        method: 'POST',
+        path :'/lista',
+        handler:function(request, h){
+        console.log(request.payload);
+            lista.push(request.payload.data);
+            return {data: lista};
+            }
+    },
+    {
+        method: 'PUT',
+        path :'/lista/{index}',
+        handler:function(request, h){
+            lista[request.params.index]  = request.payload.data;
+            return {data: lista};
+                 }
+     },{
+        method: 'DELETE',
+        path :'/lista/{index}',
+        handler:function(request, h){
+            lista.splice[request.params.index];
+            return {data: lista};
+                 }
+     }
+]
